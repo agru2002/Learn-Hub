4e5f8bfdf64be1ebd2f4e599a1ae6e485f0845e4
Ankit Sharma <56446285+agru2002@users.noreply.github.com>
2024-03-18 06:51:30 +0530

    Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Landing from "./components/Landing";
-import CreateCourse from './components/CreateCourse';
-import Register from './components/Register';
-import ShowCourses from './components/ShowCourses';
+import Signin from "./Signin.jsx";
+import Signup from "./Signup.jsx";
+import Appbar from "./Appbar.jsx";
+import AddCourse from "./AddCourse.jsx";
+import Courses from "./Courses";
+import Course from "./Course";
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+  } from 'recoil';
 
-// This file shows how you can do routing in React.
-// Try going to /login, /register, /about, /courses on the website and see how the html changes
-// based on the route.
-// You can also try going to /random and see what happens (a route that doesnt exist)
 function App() {
+
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/about" element={<CreateCourse />} />
-                <Route path="/courses" element={<ShowCourses />} />
-            </Routes>
-        </Router>
+        <div style={{width: "100vw",
+            height: "100vh",
+            backgroundColor: "#eeeeee"}}
+        >
+            <RecoilRoot>
+                <Router>
+                    <Appbar />
+                    <Routes>
+                    <Route path={"/addcourse"} element={<AddCourse />} />
+                    <Route path={"/course/:courseId"} element={<Course />} />
+                    <Route path={"/courses"} element={<Courses />} />
+                    <Route path={"/signin"} element={<Signin />} />
+                    <Route path={"/signup"} element={<Signup />} />
+                    </Routes>
+                </Router>
+            </RecoilRoot>
+        </div>
     );
 }
 
